b8871ede8a05ea42d7f160a2b88843df456f9766
Samson Ikilama <mayindiikilama@gmail.com>
2026-06-02 22:05:58 -0400

    Add AppDbContext for EF Core database integration

---
+++ SIVehicleMaintenance/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SIVehicleMaintenance.Models;
+
+namespace SIVehicleMaintenance.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MaintenanceRecord> MaintenanceRecords { get; set; }
+    }
+}
+
