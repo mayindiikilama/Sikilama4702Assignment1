e549255b028f60168eb58c161671eeee4d842d53
Samson Ikilama <mayindiikilama@gmail.com>
2026-06-02 22:01:22 -0400

    Added a new MaintenanceRecord.cs file, introducing the MaintenanceRecord class in the SIVehicleMaintenance.Models namespace. This class models vehicle maintenance records with properties for ID, vehicle name, service type, cost, and service date.

---
+++ SIVehicleMaintenance/Models/MaintenanceRecord.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SIVehicleMaintenance.Models
+{
+    public class MaintenanceRecord
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string VehicleName { get; set; } = string.Empty;
+
+        [Required]
+        public string ServiceType { get; set; } = string.Empty;
+
+        [Range(0, 10000)]
+        public decimal Cost { get; set; }
+
+        public DateTime ServiceDate { get; set; }
+    }
+}
